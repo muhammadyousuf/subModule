4bc9bef32b851cfdb412f69e304aa6ea846f55e8
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2021-08-12 00:41:05 +0500

    implement material design

--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -1,5 +1,9 @@
-/// <reference types="react" />
-declare const SayHello: ({ name }: {
-    name: string;
-}) => JSX.Element;
+import React from "react";
+interface component {
+    title?: string;
+    onPress?: () => void;
+    type?: any;
+    formName?: string;
+}
+declare const SayHello: React.FC<component>;
 export default SayHello;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Button, TextField, Typography } from '@material-ui/core';
 
-const SayHello = ({ name }: { name: string }): JSX.Element => (
-  <div>Hey {name}, TypeScript1.</div>
-);
 
-export default SayHello;
+
+interface component {
+  title?: string,
+  onPress?: () => void,
+  type?: any,
+  formName?: string
+}
+const SayHello: React.FC<component> = ({ formName, title, onPress, type }) => {
+  return (
+    <div style={{
+      marginLeft: "-100px",
+      marginTop: "-100px",
+      top: "50%",
+      left: "50%",
+      position: "fixed"
+
+    }} >
+      <Typography variant="h4" component="h4" >
+        {formName}
+      </Typography>
+      <form noValidate autoComplete="off">
+        <TextField id="name" label="username" />
+        <br />
+        <TextField id="email" label="email" type="email" />
+        <br />
+        <TextField id="password" label="password" type="password" />
+        <br /><br /><br />
+        <Button variant="contained" color={type} onClick={onPress} >
+          {title}
+        </Button>
+      </form>
+
+    </div >
+  )
+};
+export default SayHello
